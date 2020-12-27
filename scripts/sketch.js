b64ffc989c7ec3f68ea57f9cfa5029b5c074de22
“ericelvendahl” <ericelvendahl@gmail.com>
2020-12-27 01:17:17 -0600

    basic flight mechanics functional

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,8 +1,10 @@
+//- jQuery test
 // $(document).ready(function () {
 //   $("#my-div").click(function () {
 //     $(this).hide();
 //   });
 // });
+
 let ship;
 function setup() {
   createCanvas(windowWidth * 0.75, windowHeight);
@@ -21,6 +23,7 @@ function draw() {
 
 function keyReleased() {
   ship.setRotation(0);
+  ship.boosting(false);
 }
 
 function keyPressed() {
@@ -29,7 +32,7 @@ function keyPressed() {
   } else if (keyCode == LEFT_ARROW) {
     ship.setRotation(-0.1);
   } else if (keyCode == UP_ARROW) {
-    ship.boost();
+    ship.boosting(true);
   }
 }
 
@@ -39,15 +42,24 @@ function Ship() {
   this.heading = 0;
   this.rotation = 0;
   this.vel = createVector(0, 0);
+  this.isBoosting = false;
+
+  this.boosting = function (b) {
+    this.isBoosting = b;
+  };
 
   this.boost = function () {
     let force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
     this.vel.add(force);
   };
 
   this.update = function () {
+    if (this.isBoosting) {
+      this.boost();
+    }
     this.pos.add(this.vel);
-    this.vel.mult(0.95);
+    this.vel.mult(0.99);
   };
 
   this.render = function () {
