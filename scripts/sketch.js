19fab6bacddbf737c4a556c00c6968a83bd7c39f
“ericelvendahl” <ericelvendahl@gmail.com>
2020-12-27 01:38:10 -0600

    single circle asteroid generation functional

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -6,9 +6,12 @@
 // });
 
 let ship;
+let asteroids = [];
+
 function setup() {
   createCanvas(windowWidth * 0.75, windowHeight);
   ship = new Ship();
+  asteroids.push(new Asteroid());
 }
 
 function draw() {
@@ -17,6 +20,10 @@ function draw() {
   ship.turn();
   ship.update();
   ship.edges();
+
+  for (let i = 0; i < asteroids.length; i++) {
+    asteroids[i].render();
+  }
 
   //- Auto-turn for debugging
   //ship.turn(0.1);
@@ -35,60 +42,4 @@ function keyPressed() {
   } else if (keyCode == UP_ARROW) {
     ship.boosting(true);
   }
-}
-
-function Ship() {
-  this.pos = createVector(width / 2, height / 2);
-  this.r = 20;
-  this.heading = 0;
-  this.rotation = 0;
-  this.vel = createVector(0, 0);
-  this.isBoosting = false;
-
-  this.boosting = function (b) {
-    this.isBoosting = b;
-  };
-
-  this.boost = function () {
-    let force = p5.Vector.fromAngle(this.heading);
-    force.mult(0.1);
-    this.vel.add(force);
-  };
-
-  this.update = function () {
-    if (this.isBoosting) {
-      this.boost();
-    }
-    this.pos.add(this.vel);
-    this.vel.mult(0.99);
-  };
-
-  this.render = function () {
-    translate(this.pos.x, this.pos.y);
-    rotate(this.heading + PI / 2);
-    noFill();
-    stroke(255);
-    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
-  };
-
-  this.edges = function () {
-    if (this.pos.x > width + this.r) {
-      this.pos.x = -this.r;
-    } else if (this.pos.x < -this.r) {
-      this.pos.x = width + this.r;
-    }
-    if (this.pos.y > height + this.r) {
-      this.pos.y = -this.r;
-    } else if (this.pos.y < -this.r) {
-      this.pos.y = height + this.r;
-    }
-  };
-
-  this.setRotation = function (a) {
-    this.rotation = a;
-  };
-
-  this.turn = function (anle) {
-    this.heading += this.rotation;
-  };
 }
